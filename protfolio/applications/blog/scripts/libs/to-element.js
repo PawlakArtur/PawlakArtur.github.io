84020c559c6e7cf43e2c59b043b1ec360f921f8d
Artur Pawlak <pawlak.artur90@gmail.com>
2017-02-17 07:33:46 +0100

    remove all files

--- protfolio/applications/blog/scripts/libs/to-element.js
+++
@@ -1,5 +0,0 @@
-export function toElement(compiled) {
-    let parser = new DOMParser();
-    let $document = parser.parseFromString(compiled, 'text/html');
-    return $document.querySelector('body').firstElementChild;
-}
